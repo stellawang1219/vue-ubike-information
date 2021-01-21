a5f4f4a16e04c6dbe893c4e18958b60ef17e64c1
stellawang1219 <stellawang1219@gmail.com>
2021-01-21 18:14:33 +0800

    Create Search.vue

---
+++ ubike-app/src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="searchBar">
+    <p>站點名稱搜尋: <input type="text" v-model="searchName" /></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    //parentMessage: String
+  },
+  data() {
+    return {
+      searchName: "",
+    };
+  },
+  watch: {
+    searchName() {
+      console.log("search");
+      this.$emit("search", this.searchName);
+    },
+  },
+};
+</script>
+
+<style>
+#searchBar{
+  text-align :left;
+  margin-left :20px;
+}
+</style>
